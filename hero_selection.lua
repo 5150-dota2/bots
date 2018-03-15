b49e16dce57451c87711011e2135e2449821ee1f
Steven Than <steventhan11@gmail.com>
2018-03-14 23:53:40 -0400

    hero selection

---
+++ hero_selection.lua
@@ -0,0 +1,21 @@
+function Think()
+  if (GetGameMode() == GAMEMODE_1V1MID) then
+    local players = GetTeamPlayers(GetTeam());
+    for i = 0, #players do
+      print(players[i]);
+    end
+    -- Team radiant
+  	SelectHero(2, "npc_dota_hero_nevermore");
+  	SelectHero(3, "npc_dota_hero_crystal_maiden");
+  	SelectHero(4, "npc_dota_hero_crystal_maiden");
+  	SelectHero(5, "npc_dota_hero_crystal_maiden");
+  	SelectHero(6, "npc_dota_hero_crystal_maiden");
+
+    -- Team dire
+  	SelectHero(7, "npc_dota_hero_nevermore");
+  	SelectHero(8, "npc_dota_hero_crystal_maiden");
+  	SelectHero(9, "npc_dota_hero_crystal_maiden");
+  	SelectHero(10, "npc_dota_hero_crystal_maiden");
+  	SelectHero(11, "npc_dota_hero_crystal_maiden");
+  end
+end
